dc33deb74e44520347662a47fe5afb64f624a3e9
Hieu Tran <hmtran.ae@gmail.com>
2019-02-21 08:50:24 -0500

    move project from vscode to jetbrains rider

---
+++ Grades.Tests/GradeBookTests.cs
@@ -0,0 +1,44 @@
+﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//
+//namespace Grades.Tests
+//{
+//    [TestClass]
+//    public class UnitTest1
+//    {
+//        [TestMethod]
+//        public void TestMethod1()
+//        {
+//            Assert.AreEqual(3, 3);
+//        }
+//    }
+//}
+
+using NUnit.Framework;
+
+namespace Grades.Tests
+{
+    [TestFixture]
+    public class GradeBookTests
+    {
+        [Test]
+        public void ComputesHighestGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(10);
+            book.AddGrade(90);
+
+            GradeStatistics results = book.ComputeStatistics();
+            Assert.AreEqual(90, results.HighestGrade);
+        }
+        [Test]
+        public void ComputesLowestGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(10);
+            book.AddGrade(90);
+
+            GradeStatistics results = book.ComputeStatistics();
+            Assert.AreEqual(10, results.LowestGrade);
+        }
+    }
+}
