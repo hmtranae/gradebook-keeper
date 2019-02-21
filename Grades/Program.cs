339b7797de4ddb9d66d30632db564ee0429cccf9
Hieu Tran <hmtran.ae@gmail.com>
2019-02-21 15:07:42 -0500

    define static method WriteResult that adds description to grade stats

--- Grades/Program.cs
+++ Grades/Program.cs
@@ -1,24 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Grades
-{
-    class Program
-    {
-        static void Main(string[] args) 
-        {
-            GradeBook book = new GradeBook();
-            book.AddGrade(91);
-            book.AddGrade(75);
-            book.AddGrade(89.5f);
-
-            GradeStatistics stats = book.ComputeStatistics();
-            Console.WriteLine(stats.AverageGrade);
-            Console.WriteLine(stats.HighestGrade);
-            Console.WriteLine(stats.LowestGrade);
-        }
-    }
-}
+﻿using System;using System.Collections.Generic;using System.Linq;using System.Text;using System.Threading.Tasks;namespace Grades{    class Program    {        static void Main(string[] args)         {            GradeBook book = new GradeBook();            book.AddGrade(91);            book.AddGrade(75);            book.AddGrade(89.5f);            GradeStatistics stats = book.ComputeStatistics();            WriteResult("Average", stats.AverageGrade);            // C# type cast (int) to change to int value            WriteResult("Highest", (int)stats.HighestGrade);            WriteResult("Lowest", stats.LowestGrade);        }        static void WriteResult(string description, int result)        {            Console.WriteLine(description + ": " + result);        }        static void WriteResult(string description, float result)        {            Console.WriteLine(description + ": " + result);        }    }}
