0e519f87c6ec3928e8928419d073511914e22482
Hieu Tran <hmtran.ae@gmail.com>
2019-02-21 11:15:57 -0500

    unit testing to understand differences between reference and value types

--- Grades.Tests/TypesTest/ReferenceTypeTests.cs
+++ Grades.Tests/TypesTest/ReferenceTypeTests.cs
@@ -1,7 +1,29 @@
+using NUnit.Framework;
+
+// namespace should match the repository name
 namespace Grades.Tests.TypesTest
 {
+    [TestFixture]
     public class ReferenceTypeTests
     {
-        
+        [Test]
+        public void GradeBookVariablesHoldAReference()
+        {
+            GradeBook g1 = new GradeBook();
+            GradeBook g2 = g1;
+
+            g1.Name = "Scott's grade book";
+            Assert.AreEqual(g1.Name, g2.Name);
+        }
+
+        [Test]
+        public void IntVariablesHoldAValue()
+        {
+            int x1 = 100;
+            int x2 = x1;
+
+            x1 = 4;
+            Assert.AreEqual(x1, x2);
+        }
     }
 }
--- Grades/Program.cs
+++ Grades/Program.cs
@@ -10,22 +10,15 @@ namespace Grades
     {
         static void Main(string[] args) 
         {
-            GradeBook g1 = new GradeBook();
-            GradeBook g2 = g1;
-
-            g1.Name = "Scott's grade book";
-            Console.WriteLine(g2.Name);
-            /*
             GradeBook book = new GradeBook();
             book.AddGrade(91);
-            book.AddGrade(89.5f);
             book.AddGrade(75);
+            book.AddGrade(89.5f);
 
             GradeStatistics stats = book.ComputeStatistics();
             Console.WriteLine(stats.AverageGrade);
             Console.WriteLine(stats.HighestGrade);
             Console.WriteLine(stats.LowestGrade);
-            */
         }
     }
 }
